6a1b467052d584b09bcfe219d0568257efb7c546
Mekala-Savitha <mekalasavitha202@gmail.com>
2026-06-25 11:00:40 +0530

    Create main.py

---
+++ Simple-Interest-Module/main.py
@@ -0,0 +1,6 @@
+import interest 
+p=float(input("Enter Principal Amount:"))
+r=float(input("Enter Rate of Interest:"))
+t=float(input("Enter Time:"))
+result=interest.simple_interest(p,r,t)
+print("Simple Interest:",result)
